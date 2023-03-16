d9c923acf9cf82770e010d80a04de00b88f3ab6b
おもちくん <sei.t11@icloud.com>
2023-03-16 13:11:37 +0900

    update shell move

--- main.c
+++ main.c
@@ -6,13 +6,27 @@ int cal_jointed_size(char* A,char* B){
     return (strchr(A,'\0') - A) + (strchr(B,'\0') - B);
 }
 
-
-// すでに存在している場合はエラー
 int shell_move(char* from_dir,char* to_dir,char* name){
-    char path[cal_jointed_size(from_dir,name)];
+    if(path_exists(to_dir) == false){
+        return -1;
+    }
 
-    sprintf(path,"%s%s",from_dir,name);
+    char from_path[cal_jointed_size(from_dir,name)];
+    char to_path  [cal_jointed_size(to_dir  ,name)];
 
+    sprintf(from_path,"%s%s",from_dir,name);
+    sprintf(to_path  ,"%s%s",to_dir  ,name);
+
+    if(path_exists(from_path) == false){
+        return -1;
+    }
+    if(path_exists(to_path)){
+        return -1;
+    }
+    
+    rename(from_path,to_path);
+
+    return 0;
 }
 
 int main(int argc, char const *argv[]){
