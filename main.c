eb99c087916ce2d806b96d325902102eddff5652
おもちくん <sei.t11@icloud.com>
2023-03-16 12:59:23 +0900

    add join string

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]){
+int join_string(char* A,char* B){
+    int buf_size = (strchr(A,'\0') - &A) + 
+                   (strchr(B,'\0') - &B);
     
+    char path[buf_size];
+
+    sprintf(path,"%s%s",A,B);
+    printf("%s\n",path);
+}
+
+int main(int argc, char const *argv[]){
+    join_string("hello"," world");
+
     return 0;
 }
