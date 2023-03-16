c99aa55874fadd2f924cb8f03a74b3195e787653
おもちくん <sei.t11@icloud.com>
2023-03-16 13:03:41 +0900

    add shell move

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int join_string(char* A,char* B){
-    int buf_size = (strchr(A,'\0') - A) + 
-                   (strchr(B,'\0') - B);
-    
-    char path[buf_size];
-
-    sprintf(path,"%s%s",A,B);
-    printf("%s\n",path);
 
 int cal_jointed_size(char* A,char* B){
     return (strchr(A,'\0') - A) + (strchr(B,'\0') - B);
 }
 
+
+// すでに存在している場合はエラー
+int shell_move(char* from_dir,char* to_dir,char* name){
+    char path[cal_jointed_size(from_dir,name)];
+
+    sprintf(path,"%s%s",from_dir,name);
+
+}
 
 int main(int argc, char const *argv[]){
     join_string("hello"," world");
