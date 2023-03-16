3ded7239bfcd98e71a643aa575271527556bda82
おもちくん <sei.t11@icloud.com>
2023-03-16 13:00:47 +0900

    fix error

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int join_string(char* A,char* B){
-    int buf_size = (strchr(A,'\0') - &A) + 
-                   (strchr(B,'\0') - &B);
+    int buf_size = (strchr(A,'\0') - A) + 
+                   (strchr(B,'\0') - B);
     
     char path[buf_size];
 
     sprintf(path,"%s%s",A,B);
     printf("%s\n",path);
+
+    return 0;
 }
 
 int main(int argc, char const *argv[]){
